ece65a7689e393128328f2156e776149ce550210
Peter Kieltyka <peter.kieltyka@gmail.com>
2023-10-28 18:22:02 -0400

    add With(...) method

--- logger.go
+++ logger.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Logger interface {
+	With(args ...interface{}) Logger
 	Debug(v ...interface{})
 	Debugf(format string, v ...interface{})
 	Info(v ...interface{})
@@ -45,67 +46,128 @@ var (
 type StdLogAdapter struct {
 	Level Level
 	out   *log.Logger
+	attrs string
 }
 
 func NewLogger(level Level) Logger {
 	return &StdLogAdapter{out: log.New(os.Stdout, "", 0), Level: level}
 }
 
+func (s *StdLogAdapter) With(args ...interface{}) Logger {
+	if len(args) == 0 {
+		return s
+	}
+	if len(args)%2 != 0 {
+		return s
+	}
+	attrs := ""
+	for i := 0; i < len(args); i += 2 {
+		k := args[i]
+		v := args[i+1]
+		attrs += fmt.Sprintf("%s:%v", k, v)
+		if i+2 < len(args) {
+			attrs += ", "
+		}
+	}
+	out := &StdLogAdapter{Level: s.Level, out: s.out, attrs: fmt.Sprintf("[%s]", attrs)}
+	return out
+}
+
 func (s *StdLogAdapter) Debug(v ...interface{}) {
 	if s.Level <= LogLevel_DEBUG {
-		s.Println(append([]interface{}{"[DEBUG]"}, v...)...)
+		if s.attrs != "" {
+			s.Println(append([]interface{}{"[DEBUG]"}, append([]interface{}{s.attrs}, v...)...)...)
+		} else {
+			s.Println(append([]interface{}{"[DEBUG]"}, v...)...)
+		}
 	}
 }
 
 func (s *StdLogAdapter) Debugf(format string, v ...interface{}) {
 	if s.Level <= LogLevel_DEBUG {
-		s.Printf(fmt.Sprintf("[DEBUG] %s", format), v...)
+		if s.attrs != "" {
+			s.Printf(fmt.Sprintf("[DEBUG] %v %s", s.attrs, format), v...)
+		} else {
+			s.Printf(fmt.Sprintf("[DEBUG] %s", format), v...)
+		}
 	}
 }
 
 func (s *StdLogAdapter) Info(v ...interface{}) {
 	if s.Level <= LogLevel_INFO {
-		s.Println(append([]interface{}{"[INFO]"}, v...)...)
+		if s.attrs != "" {
+			s.Println(append([]interface{}{"[INFO]"}, append([]interface{}{s.attrs}, v...)...)...)
+		} else {
+			s.Println(append([]interface{}{"[INFO]"}, v...)...)
+		}
 	}
 }
 
 func (s *StdLogAdapter) Infof(format string, v ...interface{}) {
 	if s.Level <= LogLevel_INFO {
-		s.Printf(fmt.Sprintf("[INFO] %s", format), v...)
+		if s.attrs != "" {
+			s.Printf(fmt.Sprintf("[INFO] %v %s", s.attrs, format), v...)
+		} else {
+			s.Printf(fmt.Sprintf("[INFO] %s", format), v...)
+		}
 	}
 }
 
 func (s *StdLogAdapter) Warn(v ...interface{}) {
 	if s.Level <= LogLevel_WARN {
-		s.Println(append([]interface{}{"[WARN]"}, v...)...)
+		if s.attrs != "" {
+			s.Println(append([]interface{}{"[WARN]"}, append([]interface{}{s.attrs}, v...)...)...)
+		} else {
+			s.Println(append([]interface{}{"[WARN]"}, v...)...)
+		}
 	}
 }
 
 func (s *StdLogAdapter) Warnf(format string, v ...interface{}) {
 	if s.Level <= LogLevel_WARN {
-		s.Printf(fmt.Sprintf("[WARN] %s", format), v...)
+		if s.attrs != "" {
+			s.Printf(fmt.Sprintf("[WARN] %v %s", s.attrs, format), v...)
+		} else {
+			s.Printf(fmt.Sprintf("[WARN] %s", format), v...)
+		}
 	}
 }
 
 func (s *StdLogAdapter) Error(v ...interface{}) {
 	if s.Level <= LogLevel_ERROR {
-		s.Println(append([]interface{}{"[ERROR]"}, v...)...)
+		if s.attrs != "" {
+			s.Println(append([]interface{}{"[ERROR]"}, append([]interface{}{s.attrs}, v...)...)...)
+		} else {
+			s.Println(append([]interface{}{"[ERROR]"}, v...)...)
+		}
 	}
 }
 
 func (s *StdLogAdapter) Errorf(format string, v ...interface{}) {
 	if s.Level <= LogLevel_ERROR {
-		s.Printf(fmt.Sprintf("[ERROR] %s", format), v...)
+		if s.attrs != "" {
+			s.Printf(fmt.Sprintf("[ERROR] %v %s", s.attrs, format), v...)
+		} else {
+			s.Printf(fmt.Sprintf("[ERROR] %s", format), v...)
+		}
 	}
 }
 
 func (s *StdLogAdapter) Fatal(v ...interface{}) {
-	s.Println(append([]interface{}{"[FATAL]"}, v...)...)
+	if s.attrs != "" {
+		s.Println(append([]interface{}{"[FATAL]"}, append([]interface{}{s.attrs}, v...)...)...)
+	} else {
+		s.Println(append([]interface{}{"[FATAL]"}, v...)...)
+	}
 	os.Exit(1)
 }
 
 func (s *StdLogAdapter) Fatalf(format string, v ...interface{}) {
-	s.Printf(fmt.Sprintf("[FATAL] %s", format), v...)
+	if s.attrs != "" {
+		s.Printf(fmt.Sprintf("[FATAL] %v %s", s.attrs, format), v...)
+	} else {
+		s.Printf(fmt.Sprintf("[FATAL] %s", format), v...)
+	}
 	os.Exit(1)
 }
 
